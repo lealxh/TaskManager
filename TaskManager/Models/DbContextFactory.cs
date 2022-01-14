5e8b46723b16b5aaddc2514fa8287649ce1b899e
Jose Leal <lealxh@gmail.com>
2022-01-13 21:25:12 -0300

    Add project files.

---
+++ TaskManager/Models/DbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using TaskManager.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models
+{
+    public class DbContextFactory : IDbContextFactory
+    {
+
+        public DbContextFactory(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public DataContext Create()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+               .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")).Options;
+
+            return new DataContext(options);
+        }
+    }
+}
---
+++ TaskManager/Models/TaskData.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Models
+{
+    public enum state { Running, Canceled};
+    public class TaskData
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TotalWork { get; set; }
+        public int CurrentWork { get; set; }
+        public string State { get; set; }
+    }
+}
